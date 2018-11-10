868e5d66816edfaa21e5d56612886504cdfd894a
lwg82 <shuangyan5230@163.com>
2018-11-10 12:24:50 +0800

    binary tree add some function

--- chapter5/binary_tree.h
+++ chapter5/binary_tree.h
@@ -24,12 +24,35 @@ public:
 	binary_tree();
 	~binary_tree();
 
+	binary_tree(const binary_tree<T>& rhs);
+
 	void create_binary_tree(char *string_list);
 	void destroy_binary_tree(binary_tree_node<T> *&sub);
 
+	void preOrder(void(*visit)(binary_tree_node<T> *node));
+	void inOrder(void(*visit)(binary_tree_node<T> *node));
+	void postOrder(void(*visit)(binary_tree_node<T> *node));
+
+	int size()  { return size(root); }
+	int height()  { return height(root); };
+
+	
+
+private:
+
+	void preOrder(binary_tree_node<T> *sub, void(*visit)(binary_tree_node<T> *node));
+	void inOrder(binary_tree_node<T> *sub, void(*visit)(binary_tree_node<T> *node));
+	void postOrder(binary_tree_node<T> *sub, void(*visit)(binary_tree_node<T> *node));
+
+	int size(binary_tree_node<T> *sub);
+	int height(binary_tree_node<T> *sub);
+
+	binary_tree_node<T> * copy(binary_tree_node<T> *sub);
+
+	
+
 	// 前序遍历输出
 	void traverse(binary_tree_node<T> *sub, std::ostream &out);
-
 
 	friend std::istream& operator>>(std::istream &in, binary_tree<T> &tree)
 	{
@@ -43,7 +66,7 @@ public:
 		return out;
 	}
 
-private:
+public:
 	binary_tree_node<T> *root;
 
 	char eof;                           // 结束符 #
@@ -146,4 +169,141 @@ void binary_tree<T>::traverse(binary_tree_node<T> *sub, std::ostream &out)
 		traverse(sub->left, out);
 		traverse(sub->right, out);
 	}
+}
+
+
+template<typename T>
+void binary_tree<T>::preOrder(void(*visit)(binary_tree_node<T> *node))
+{
+	preOrder(root, visit);
+}
+
+
+template<typename T>
+void binary_tree<T>::inOrder(void(*visit)(binary_tree_node<T> *node))
+{
+	inOrder(root, visit);
+}
+
+
+template<typename T>
+void binary_tree<T>::postOrder(void(*visit)(binary_tree_node<T> *node))
+{
+	postOrder(root, visit);
+}
+
+
+template<typename T>
+void binary_tree<T>::preOrder(binary_tree_node<T> *sub, void(*visit)(binary_tree_node<T> *node))
+{
+	if (sub != nullptr)
+	{
+		visit(sub);
+		preOrder(sub->left, visit);
+		preOrder(sub->right, visit);
+	}
+
+}
+
+
+
+template<typename T>
+void binary_tree<T>::inOrder(binary_tree_node<T> *sub, void(*visit)(binary_tree_node<T> *node))
+{
+	if (sub != nullptr)
+	{
+		inOrder(sub->left, visit);
+		visit(sub);
+		inOrder(sub->right, visit);
+	}
+}
+
+
+
+template<typename T>
+void binary_tree<T>::postOrder(binary_tree_node<T> *sub, void(*visit)(binary_tree_node<T> *node))
+{
+	if (sub != nullptr)
+	{
+		postOrder(sub->left, visit);
+		postOrder(sub->right, visit);
+
+		visit(sub);
+	}
+	
+}
+
+
+
+template<typename T>
+int binary_tree<T>::size(binary_tree_node<T> *sub)
+{
+	if (nullptr == sub)
+		return 0;
+	else
+		return size(sub->left) + size(sub->right) + 1;
+}
+
+template<typename T>
+int binary_tree<T>::height(binary_tree_node<T> *sub)
+{
+	if (nullptr == sub)
+		return 0;
+	else
+	{
+		int left = height(sub->left);
+		int right = height(sub->right);
+		return left > right ? left+1 : right+1;
+	}
+}
+
+
+
+template<typename T>
+binary_tree<T>::binary_tree(const binary_tree<T> &rhs)
+{
+	root = copy(rhs.root); // 注意赋值给 root
+}
+
+template<typename T>
+binary_tree_node<T>* binary_tree<T>::copy(binary_tree_node<T> *sub)
+{
+	if (nullptr == sub)
+		return nullptr;
+
+	binary_tree_node<T> *node = new binary_tree_node<T>;
+	// 先序
+	node->data = sub->data;
+
+	node->left = copy(sub->left);
+	node->right = copy(sub->right);
+
+	return node;
+}
+
+template<typename T>
+bool equal(binary_tree_node<T> *node1, binary_tree_node<T> *node2)
+{
+	if (nullptr == node1 && nullptr == node2)
+		return true;
+	
+	if (nullptr != node1 && nullptr != node2 && node1->data == node2->data
+		&& equal(node1->left, node2->left)           // 左子树相等
+		&& equal(node1->right, node2->right))        // 右子树相等
+		return true;
+
+	return false;
+}
+
+// 非成员函数
+template<typename T>
+bool operator==(const binary_tree<T> &t1,const binary_tree<T> &t2)
+{
+	return equal(t1.root, t2.root);
+}
+
+template<typename T>
+bool operator!=(const binary_tree<T> &t1, const binary_tree<T> &t2)
+{
+	return !(t1 == t2);
 }
--- chapter5/main.cpp
+++ chapter5/main.cpp
@@ -5,6 +5,12 @@
 
 #include "binary_tree.h"
 
+template<typename T>
+void visit(binary_tree_node<T>* node)
+{
+	std::cout << node->data;
+}
+
 void test_binary_tree()
 {
 	binary_tree<char> tree;
@@ -13,9 +19,36 @@ void test_binary_tree()
 	//std::cin >> tree;
 
 	// 103
-	tree.create_binary_tree("A(B(D,E(G,)),C(,F))#");
-	
-	std::cout << tree;
+	// tree.create_binary_tree("A(B(D,E(G,)),C(,F))#");
+	//std::cout << tree;
+
+/*
+	tree.create_binary_tree("-(+(a,*(b,-(c,d))),/(e,f))#");
+
+	tree.preOrder(visit);
+	std::cout << std::endl;
+
+	tree.inOrder(visit);
+	std::cout << std::endl;
+
+	tree.postOrder(visit);
+	std::cout << std::endl;
+
+	std::cout << "size=" << tree.size() << std::endl;
+	std::cout << "height=" << tree.height() << std::endl;
+*/
+
+	binary_tree<char> tree1;
+	tree1.create_binary_tree("A(B(D,E(G,)),C(,F))#");
+	std::cout << tree1;
+	std::cout << std::endl;
+
+	binary_tree<char> tree2(tree1);
+	std::cout << tree2;
+	std::cout << std::endl;
+
+	std::cout << ((tree1 == tree2)? "t":"f") << std::endl;
+	std::cout << ((tree1 != tree2) ? "t" : "f") << std::endl;
 }
 
 int main(int argc, char *argv[])
